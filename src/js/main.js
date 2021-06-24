5bbb481d052a71cc62afd33fb2cc464c6d124591
Ronald <yuraronin@gmail.com>
2021-06-24 16:48:35 +0300

    Сделал слайдер и адаптив секций

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,59 @@
 let tab = function () {
   let tabNav = document.querySelectorAll('.tabs-list__button'),
-      tabContent = document.querySelectorAll('.tabs-content'),
-      tabName;
+    tabContent = document.querySelectorAll('.tabs-content'),
+    tabName;
 
   tabNav.forEach(item => {
-      item.addEventListener('click', selectTabNav)
+    item.addEventListener('click', selectTabNav)
   });
 
   function selectTabNav() {
-      tabNav.forEach(item => {
-          item.classList.remove('is-active');
-      });
-      this.classList.add('is-active');
-      tabName = this.getAttribute('data-tab-name');
-      selectTabContent(tabName);
+    tabNav.forEach(item => {
+      item.classList.remove('is-active');
+    });
+    this.classList.add('is-active');
+    tabName = this.getAttribute('data-tab-name');
+    selectTabContent(tabName);
   }
 
   function selectTabContent(tabName) {
-      tabContent.forEach(item => {
-          item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
-      })
+    tabContent.forEach(item => {
+      item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
+    })
   }
 
 };
 
 
 tab();
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
+});
